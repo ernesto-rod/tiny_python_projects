463c904fe9a1b92c3f2622eb884a9f43c2a8a439
Ernesto Antonio Rodriguez <ernesto.antonio.rod@gmail.com>
2023-11-09 12:14:13 -0600

    Replace for loop with list comprehension to process and transform the chars in the string

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -32,9 +32,7 @@ def main():
     jumper = {'1': '9', '2': '8', '3': '7', '4': '6', '5': '0',
               '6': '4', '7': '3', '8': '2', '9': '1', '0': '5'}
 
-    for char in args.text:
-        print(jumper.get(char, char), end='')
-    print()
+    print(''.join([jumper.get(char, char) for char in args.text]))
 
 
 # --------------------------------------------------
